28e65bdd4fb3a42e78da87ddc1b4f263af60ffe9
Manuel Perez <manuepeva@gmail.com>
2025-09-17 10:39:24 +0200

    deploying server

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:4000", 
+    "https://portfolio-server-beta-taupe.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
+
+// Ruta del formulario
+app.post("/contact", async (req, res) => {
+  const { firstName, lastName, email, date, comments } = req.body;
+
+  if (!firstName || !lastName || !email || !comments) {
+    return res.status(400).json({ success: false, msg: "Missing fields" });
+  }
+
+  try {
+    await axios.post(
+      "https://api.resend.com/emails",
+      {
+        from: `Portfolio <onboarding@resend.dev>`,
+        to: [process.env.EMAIL_TO],             
+        subject: `New message from: ${firstName} ${lastName}`,
+        text: `From: ${email}\nDate: ${date}\nComments: ${comments}`,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.json({ success: true, msg: "Successfully sent message ✅" });
+  } catch (err) {
+    console.error("Resend error:", err.response?.data || err.message);
+    res.status(500).json({ success: false, msg: "There was an error ❌" });
+  }
+});
+
+// Puerto dinámico
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+
+
+export default app;
